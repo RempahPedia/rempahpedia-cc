2eb36fcab4875597a3c6fa80e66f9f59b46f3ec3
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2024-06-13 16:36:32 +0700

    fix:remove comment

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const router = require("./routes");
 const cookieParser = require('cookie-parser');
 require("dotenv").config();
 
-// Import Routes
-// const userRoutes = require('./routes/userRoutes');
+
 
 const devName1 = process.env.DEV_NAME_1
 const devName2 = process.env.DEV_NAME_2
@@ -19,7 +18,5 @@ app.get('/', (req, res) => {
     res.send(`Hello World!, this app is developed by: ${devName1} and ${devName2}`);
 });
 
-// Routes Middleware
-// app.use('/api/users', userRoutes);
 
 module.exports = app;
