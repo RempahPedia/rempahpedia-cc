b87d1316737059c16c70f8a7801a8dae5fe1de1f
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2024-06-14 14:06:05 +0700

    fix: change response status code for better representation

--- src/routes/penggunaRempahRoutes.js
+++ src/routes/penggunaRempahRoutes.js
@@ -6,12 +6,12 @@ const penggunaService = require('../services/penggunaService');
 router.post('/save', async (req, res) => {
     try{
         if(!req.user){
-            res.status(400).json({ error: 'User Not found' });
+            res.status(401).json({ error: 'User Not found' });
         }
         const userEmail = req.user.email;
         const rempah = req.body.rempah;
         await penggunaService.savePrediciton(userEmail, rempah);
-        res.status(200).json({ 
+        res.status(201).json({ 
             message: `successfully save the prediction for user: ${userEmail}`
         });
     } catch(error){
