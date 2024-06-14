13f755fd22c50c6034f29bfb6d74373e0cc11d3b
fatah633 <fatah.alkhadami17@gmail.com>
2024-06-14 23:27:06 +0700

    Testing Insert User to SQL

--- src/services/penggunaService.js
+++ src/services/penggunaService.js
@@ -1,5 +1,23 @@
 const db = require('../config/dbconnection');
 
+async function saveUser(userEmail){
+    // const query = 'INSERT INTO Pengguna(email) VALUES($1)';
+    // const rows = await db.query(query, [userEmail]);
+    // return rows;
+
+    const client = await pool.connect();
+    try {
+        const query = 'INSERT INTO Pengguna (email) VALUES ($1) RETURNING *';
+        const res = await client.query(query, [email]);
+        return res.rows[0];
+    } catch (err) {
+        console.error('Error inserting data:', err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
 async function savePrediciton(userEmail, rempah){
     const rempahIdQuery = 'SELECT id FROM REMPAH WHERE nama=$1';
     const rempahIdResult = await db.query(rempahIdQuery, [rempah]);
@@ -12,4 +30,5 @@ async function savePrediciton(userEmail, rempah){
 
 module.exports = {
     savePrediciton,
+    saveUser
 };
