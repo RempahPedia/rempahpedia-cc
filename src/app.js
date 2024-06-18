5843f85516f000aad01107b07364ab5f5d6d985b
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2024-06-18 13:53:52 +0700

    fix : remove redundant routing and middleware use

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const router = require("./routes");
 const cookieParser = require('cookie-parser');
-const verifyAndRefreshToken = require('./middleware/index');
+const {verifyAndRefreshToken} = require('./middleware/index');
 require("dotenv").config();
-const protectedRoutes = require('./routes/index');
 
 // Import Routes
 const jamuRoutes = require('./routes/jamuRoutes');
@@ -14,13 +13,12 @@ const penggunaRempahRoutes = require('./routes/penggunaRempahRoutes');
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(verifyAndRefreshToken.verifyAndRefreshToken);
+app.use(verifyAndRefreshToken);
 
 // Routes 
 app.use(router);
 app.use('/api/jamu', jamuRoutes);
 app.use('/api/rempah', rempahRoutes);
 app.use('/api/prediction', penggunaRempahRoutes);
-app.use('/api', protectedRoutes);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const verifyAndRefreshToken = require('../middleware/index');
 const firebaseAuthController = require('../controllers/firebase-auth-controller');
 const penggunaService = require('../services/penggunaService')
 
@@ -8,7 +7,6 @@ router.post('/api/register', firebaseAuthController.registerUser);
 router.post('/api/login', firebaseAuthController.loginUser);
 router.post('/api/logout', firebaseAuthController.logoutUser);
 router.post('/api/reset-password', firebaseAuthController.resetPassword);
-router.use(verifyAndRefreshToken.verifyAndRefreshToken);
 router.get('/protected', (req, res) => {
     res.send(`You are authenticated!`);
 });
