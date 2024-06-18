402d6e1155db92a09a992cc84a287a59d16686bc
fatah633 <fatah.alkhadami17@gmail.com>
2024-06-18 12:05:19 +0700

    fix: idToken

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -21,7 +21,8 @@ const verifyAndRefreshToken = async (req, res, next) => {
     const idToken = req.headers.authorization?.split('Bearer ')[1];
 
     if (!idToken) {
-        return res.status(401).send('Unauthorized');
+        req.user = null; 
+        return next();
     }
 
     try {
